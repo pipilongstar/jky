03000c4c750c0366ea380d7afc6705594711e355
ZSL <2534158602@qq.com>
2023-07-23 17:56:16 +0800

    first

---
+++ src/main/java/com/pipilong/OnlineEducationApplication.java
@@ -0,0 +1,14 @@
+package com.pipilong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineEducationApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(OnlineEducationApplication.class, args);
+    }
+
+}
